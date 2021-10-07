b15a92dbdd6f21d5a9af1cd59f70f18b4b553feb
Silas de Melo Araujo <silasmelo1967@gmail.com>
2021-10-07 01:09:37 -0300

    add user routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,19 @@
 
 const express = require('express');
 
+const renderView = require('../middleware/renderView');
+
 const products = require('./products');
+const user = require('./user');
 
 
 
 const router = express.Router();
 
 router.use(products);
+router.use(user);
+
+router.use(renderView);
 
 
 
